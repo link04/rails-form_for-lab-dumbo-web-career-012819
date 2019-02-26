ee12a5af4429f841d42012be4fc51afe29056786
Maximo Bautista <max.bautista97@gmail.com>
2019-02-26 22:46:43 +0000

    Automatically backed up by Learn

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -4,11 +4,11 @@ require 'rails_helper'
 
 describe SchoolClass do
   before(:each) do
-    @school_class = SchoolClass.create!(title: "School", room_: "Targaryen")
+    @school_class = SchoolClass.create!(title: "School", room_number: 324)
   end
 
   it 'can be created' do
-    expect(@student).to be_valid
+    expect(@school_class).to be_valid
   end
 
 end
