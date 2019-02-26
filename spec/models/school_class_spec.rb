c6d0b7cb90954de77994802074e38b18ad8ff339
Maximo Bautista <max.bautista97@gmail.com>
2019-02-26 22:46:05 +0000

    Automatically backed up by Learn

--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe SchoolClass do
   before(:each) do
-    @school_class = SchoolClass.create!(title: "Daenerys", last_name: "Targaryen")
+    @school_class = SchoolClass.create!(title: "School", last_name: "Targaryen")
   end
 
   it 'can be created' do
